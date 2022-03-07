01d30d07dc7334d48b592abf05fa32f93c8923c8
justasbeikauskas <34918894+justasbeikauskas@users.noreply.github.com>
2022-03-07 15:51:59 +0200

    String, RegExp, DataTime
    Task 3

---
+++ Module_6_String_RegExp_DataTime/Task_3.js
@@ -0,0 +1,6 @@
+// Get the day, month and year of the current date and output it to the console separately
+
+let date = new Date();
+console.log("Today is the DAY of month: " + date.getDate());
+console.log("Today is MONTH: " + date.getMonth());
+console.log("Today is YEAR: " + date.getFullYear());
