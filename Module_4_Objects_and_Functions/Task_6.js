28ab32da2a5df8771a7465ef337ade11da406812
justasbeikauskas <34918894+justasbeikauskas@users.noreply.github.com>
2022-03-07 13:24:27 +0200

    Objects and Functions
    Task 6

---
+++ Module_4_Objects_and_Functions/Task_6.js
@@ -0,0 +1,9 @@
+//Write a function for calculating the type of argument and type output to the console
+
+const inputValue = (n) => console.log(`Input value is ${typeof(n)}`);
+
+inputValue(9)
+
+inputValue(false)
+
+inputValue("Wau")
