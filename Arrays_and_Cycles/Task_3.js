285a2816eada8d401413b5541019da76c8efb21a
justasbeikauskas <34918894+justasbeikauskas@users.noreply.github.com>
2022-03-07 14:24:31 +0200

    Arrays and Cycles
    Task 3

---
+++ Arrays_and_Cycles/Task_3.js
@@ -0,0 +1,5 @@
+//Given an array of the names of your friends, add the words hello to each element of the array
+
+let friends = ["Name1", "Name2", "Name3", "Name4", "Name5"];
+
+friends.forEach((item)=>{item = `Hello ${item}`});
