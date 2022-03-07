2f14b6263f302e01e65b56a7042d82d56155e0d4
justasbeikauskas <34918894+justasbeikauskas@users.noreply.github.com>
2022-03-07 14:21:23 +0200

    Arrays and Cycles
    Task 1

---
+++ Arrays_and_Cycles/Task_1.js
@@ -0,0 +1,7 @@
+//Given an array consisting of movie names, iterate over the array with the output of the names of each movie to the console
+
+let movies = ["movie1", "movie2", "movie3", "movie4"]
+
+for (let items of movies) {
+  console.log(items);
+}
