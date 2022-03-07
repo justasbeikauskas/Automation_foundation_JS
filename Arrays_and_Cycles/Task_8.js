482a4e05dc7188834dc42b73c8db0dad8600934a
justasbeikauskas <34918894+justasbeikauskas@users.noreply.github.com>
2022-03-07 15:23:17 +0200

    Arrays and Cycles
    Task 8

---
+++ Arrays_and_Cycles/Task_8.js
@@ -0,0 +1,5 @@
+// Implement a loop that will print the number 'a' until it is less than 10
+
+let numbers = [1,2,3,6,7,8,3,4,5,6,10,12];
+
+numbers.forEach(function(item){if(item<10) {alert(item)}});
