2b427867b65de6032b1a892301b426d492d12355
justasbeikauskas <34918894+justasbeikauskas@users.noreply.github.com>
2022-03-07 15:21:50 +0200

    Arrays and Cycles
    Task 7

---
+++ Arrays_and_Cycles/Task_7.js
@@ -0,0 +1,7 @@
+//Write a function that takes two parameters - an array and a number and outputs the index of an array element equal to a number
+
+let numbers = [1,2,3,6,7,8,3,4,5,6,10,12];
+
+function findingIndex(arr, item) {
+	alert(arr.indexOf(item));
+}
